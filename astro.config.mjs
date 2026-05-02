2a9cde30f358dafb76c156e69337c1d758dd0d48
DeepContaine <xander.loh@outlook.com>
2026-05-02 13:24:04 +0800

    chore(content): sync main@96d7690

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.siteURL,
+	site: 'https://deepcontaine.github.io',
 	base: "/",
 	trailingSlash: "always",
 
